c5eeece7c083bb73516beb5fa8553f8259acb26d
Simon Lindblad <simon.lindblad93@gmail.com>
2017-03-18 20:22:07 +0800

    Remove whitespace from sPL_combile.ml

--- lab07/sPL_compile.ml
+++ lab07/sPL_compile.ml
@@ -10,10 +10,10 @@ let trans_cmd op =
     | "/" -> DIV
     | "\\" -> NOT
     | "~" -> NEG
-    | "|" -> OR  
-    | "&" -> AND 
-    | "<" -> LT 
-    | ">" -> GT 
+    | "|" -> OR
+    | "&" -> AND
+    | "<" -> LT
+    | ">" -> GT
     | "=" -> EQ
     | _ -> failwith ("no such operator "^op)
 
@@ -30,7 +30,7 @@ let enum_cenv xs n =
   let rec aux xs n =
     match xs with
       | [] -> []
-      | x::xs -> (x,n)::(aux xs (n+1)) 
+      | x::xs -> (x,n)::(aux xs (n+1))
   in aux xs n
 
 (* compiling to eVML instrs *)
@@ -52,14 +52,14 @@ let compile (e:sPL_expr) : sVML_prog_sym   =
                 | Some i -> [LD (s,i)],[]
                 | None -> [LD (s,(-1))],[]
             end
-      | Func (t,vs,body) -> 
+      | Func (t,vs,body) ->
             let l_fn = labels # fresh_id in
             let fvs = diff (fv body) vs in
             let all_vs = fvs@vs in
             let new_ce = enum_cenv all_vs 0 in
             let arity = List.length vs in
             let (s1,p1) = helper new_ce body in
-            let fvs_n = List.map 
+            let fvs_n = List.map
               (fun v -> match (Environ.get_val ce v) with
                 | Some i ->(v,i)
                 | _ -> (v,-1)) fvs in
@@ -68,7 +68,7 @@ let compile (e:sPL_expr) : sVML_prog_sym   =
             failwith "TO BE IMPLEMENTED"
       | Appln (f,_,args) ->
             failwith "TO BE IMPLEMENTED"
-      | RecFunc (t,f,vs,body) -> 
+      | RecFunc (t,f,vs,body) ->
             failwith "TO BE IMPLEMENTED"
   in
   let (main_code,proc_code) = (helper [] e)
@@ -78,7 +78,7 @@ let collect_label xs =
   let rec aux xs n =
     match xs with
       | [] -> []
-      | x::xs -> 
+      | x::xs ->
             begin
             match x with
               | LABEL s -> (s,n)::(aux xs n)
@@ -87,7 +87,7 @@ let collect_label xs =
   in aux xs 0
 
 let trans env c =
-  let get_addr l = 
+  let get_addr l =
     match (Environ.get_val env l) with
       | Some a -> a
       | None -> failwith ("linking problem : unrecognised label :"^l)
@@ -99,7 +99,7 @@ let trans env c =
     | LDF (g,a,l) -> LDF (g,a,get_addr l)
     | LDFR (g,f,a,l) -> LDFR (g,f,a,get_addr l)
     | LDCI i -> LDCI i
-    | LDCB b -> LDCB b 
+    | LDCB b -> LDCB b
     | PLUS -> PLUS
     | MINUS -> MINUS
     | TIMES -> TIMES
@@ -127,7 +127,7 @@ let link_code (xs:sVML_prog_sym) : sVML_prog_mc =
   let env = collect_label xs in
       (List.map (trans env) xs)
 
-let filter_label (xs:sVML_prog_mc) : sVML_prog_mc = 
+let filter_label (xs:sVML_prog_mc) : sVML_prog_mc =
   List.filter (fun c -> match c with LABEL _ -> false | _ -> true) xs
 
 
@@ -145,7 +145,7 @@ let extract_filename (s:string) : string =
   let v = String.length s in
   if v<5 then
     failwith "filename at least one char"
-  else 
+  else
     let fn = String.sub s 0 (v-4) in
     let extn = String.sub s (v-4) 4 in
     if extn = ".spl" then fn
@@ -175,7 +175,7 @@ let main =
   (* Read the arguments of command *)
   let _ = Debug.parse option_flag in
   (* let _ = Debug.read_main () in *)
-  if String.length !VarGen.file == 0 then print_endline VarGen.usage else 
+  if String.length !VarGen.file == 0 then print_endline VarGen.usage else
     let _ = print_endline "Loading sPL program .." in
     let (s,p) = Genparse.parse_file !VarGen.file in
     let _ = print_endline ("  "^s) in
@@ -194,7 +194,7 @@ let main =
             let _ = print_string ("COMPILING ==> "^bytefn^"\n") in
             let r = compile np in
             let _ = print_endline (string_of_sVML_list_sym r) in
-            let r = 
+            let r =
               if !tail_optimize_flag then
                 let r = tail_optimize r in
                 let _ = print_endline ("TAIL-OPTIMIZE ==> ") in
